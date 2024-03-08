39925a1efd390fc366477561301b99a8f56c9b36
lemonnuggets <63046776+lemonnuggets@users.noreply.github.com>
2024-03-08 13:59:45 +0530

    fix(nvim): conform autocommands defined outside so running command loads conform

--- dot_config/nvim/lua/custom/plugins/conform.lua
+++ dot_config/nvim/lua/custom/plugins/conform.lua
@@ -1,5 +1,62 @@
+vim.api.nvim_create_user_command('Format', function(args)
+  local range = nil
+  if args.count ~= -1 then
+    local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
+    range = { start = { args.line1, 0 }, ['end'] = { args.line2, end_line:len() } }
+  end
+  require('conform').format { async = true, lsp_fallback = true, range = range }
+end, { range = true })
+
+vim.api.nvim_create_user_command('FormatDisable', function(args)
+  if args.bang then -- FormatDisable! will disable formatting just for this buffer
+    vim.b.disable_autoformat = true
+  else
+    vim.g.disable_autoformat = true
+  end
+end, {
+  desc = 'Disable autoformat-on-save (! for only buffer)',
+  bang = true,
+})
+
+vim.api.nvim_create_user_command('FormatEnable', function()
+  vim.b.disable_autoformat = false
+  vim.g.disable_autoformat = false
+end, {
+  desc = 'Re-enable autoformat-on-save',
+})
+
+vim.api.nvim_create_user_command('FormatDiff', function(e)
+  local file_name = vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf())
+
+  local lines = vim.fn.system('git diff --unified=0 ' .. file_name):gmatch '[^\n\r]+'
+  local ranges = {}
+  for line in lines do
+    vim.print('line', line)
+    if line:find '^@@' then
+      local line_nums = line:match '%+.- '
+      if line_nums:find ',' then
+        local _, _, first, second = line_nums:find '(%d+),(%d+)'
+        table.insert(ranges, { start = { tonumber(first), 0 }, ['end'] = { tonumber(first) + tonumber(second), 0 } })
+      else
+        local first = tonumber(line_nums:match '%d+')
+        table.insert(ranges, {
+          start = { first, 0 },
+          ['end'] = { first + 1, 0 },
+        })
+      end
+    end
+  end
+  local format = require('conform').format
+  for _, range in pairs(ranges) do
+    format {
+      range = range,
+    }
+  end
+end, { desc = 'Format changed lines' })
+
 local formatters = require 'custom.config.formatters'
 local slow_format_filetypes = {}
+
 return {
   'stevearc/conform.nvim',
   event = { 'BufWritePre', 'BufNewFile' },
@@ -50,58 +107,5 @@ return {
     }
 
     vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
-
-    vim.api.nvim_create_user_command('Format', function(args)
-      local range = nil
-      if args.count ~= -1 then
-        local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
-        range = { start = { args.line1, 0 }, ['end'] = { args.line2, end_line:len() } }
-      end
-      require('conform').format { async = true, lsp_fallback = true, range = range }
-    end, { range = true })
-
-    vim.api.nvim_create_user_command('FormatDisable', function(args)
-      if args.bang then -- FormatDisable! will disable formatting just for this buffer
-        vim.b.disable_autoformat = true
-      else
-        vim.g.disable_autoformat = true
-      end
-    end, {
-      desc = 'Disable autoformat-on-save (! for only buffer)',
-      bang = true,
-    })
-
-    vim.api.nvim_create_user_command('FormatEnable', function()
-      vim.b.disable_autoformat = false
-      vim.g.disable_autoformat = false
-    end, {
-      desc = 'Re-enable autoformat-on-save',
-    })
-
-    vim.api.nvim_create_user_command('FormatDiff', function()
-      local lines = vim.fn.system('git diff --unified=0'):gmatch '[^\n\r]+'
-      local ranges = {}
-      for line in lines do
-        if line:find '^@@' then
-          local line_nums = line:match '%+.- '
-          if line_nums:find ',' then
-            local _, _, first, second = line_nums:find '(%d+),(%d+)'
-            table.insert(ranges, { start = { tonumber(first), 0 }, ['end'] = { tonumber(first) + tonumber(second), 0 } })
-          else
-            local first = tonumber(line_nums:match '%d+')
-            table.insert(ranges, {
-              start = { first, 0 },
-              ['end'] = { first + 1, 0 },
-            })
-          end
-        end
-      end
-      local format = require('conform').format
-      for _, range in pairs(ranges) do
-        format {
-          range = range,
-        }
-      end
-    end, { desc = 'Format changed lines' })
   end,
 }
