8a9cc8c70e0482ced86814400f23fb29c215b680
lemonnuggets <63046776+lemonnuggets@users.noreply.github.com>
2024-03-10 18:57:31 +0530

    feat(nvim): <leader>d for Debug and <leader>c for Code

--- dot_config/nvim/lua/plugins/lspconfig.lua
+++ dot_config/nvim/lua/plugins/lspconfig.lua
@@ -9,27 +9,29 @@ local on_attach = function(_, bufnr)
     vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
   end
 
-  nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-  nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+  local builtin = require 'telescope.builtin'
+  local buf = vim.lsp.buf
 
-  nmap('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
-  nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-  nmap('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
-  nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+  nmap('<leader>cr', buf.rename, '[C]ode [R]ename')
+  nmap('<leader>ca', buf.code_action, '[C]ode [A]ction')
 
-  nmap('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
+  nmap('<leader>cs', builtin.lsp_document_symbols, '[C]ode [S]ymbols')
+  nmap('<leader>cd', builtin.lsp_type_definitions, '[C]ode Type [D]efinition')
 
-  nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+  nmap('gd', builtin.lsp_definitions, '[G]oto [D]efinition')
+  nmap('gr', builtin.lsp_references, '[G]oto [R]eferences')
+  nmap('gI', builtin.lsp_implementations, '[G]oto [I]mplementation')
+  nmap('gD', buf.declaration, '[G]oto [D]eclaration')
 
-  nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
-  nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
-  nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
+  nmap('<leader>ws', builtin.lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+  nmap('<leader>wa', buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
+  nmap('<leader>wr', buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
   nmap('<leader>wl', function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    print(vim.inspect(buf.list_workspace_folders()))
   end, '[W]orkspace [L]ist Folders')
 
-  nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
-  nmap('<M-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
+  nmap('K', buf.hover, 'Hover Documentation')
+  nmap('<M-k>', buf.signature_help, 'Signature Documentation')
 end
 
 return {
--- dot_config/nvim/lua/plugins/which-key.lua
+++ dot_config/nvim/lua/plugins/which-key.lua
@@ -19,11 +19,12 @@ return {
       ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
       ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
       ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-      ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
+      ['<leader>d'] = { name = '[D]ebug', _ = 'which_key_ignore' },
       ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
+
       ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
-      ['<leader>gh'] = { name = '[H]unk', _ = 'which_key_ignore' },
-      ['<leader>gt'] = { name = '[T]oggle', _ = 'which_key_ignore' },
+      ['<leader>gh'] = { name = '[G]it [H]unk', _ = 'which_key_ignore' },
+      ['<leader>gt'] = { name = '[G]it [T]oggle', _ = 'which_key_ignore' },
     }
 
     whichkey.register({
