074aa122db6bbce984726fc26db4b3675d2186aa
lemonnuggets <63046776+lemonnuggets@users.noreply.github.com>
2024-03-16 17:04:08 +0530

    feat(nvim): using gruvbox

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,7 +1,27 @@
+-- return {
+--   'catppuccin/nvim',
+--   name = 'catppuccin',
+--   priority = 1000,
+--   config = function()
+--     vim.cmd.colorscheme 'catppuccin-mocha'
+--   end,
+-- }
+
+-- return {
+--   'folke/tokyonight.nvim',
+--   lazy = false,
+--   priority = 1000,
+--   opts = {},
+--   config = function()
+--     vim.cmd.colorscheme 'tokyonight-moon'
+--   end,
+-- }
+
 return {
-  'rebelot/kanagawa.nvim',
+  'ellisonleao/gruvbox.nvim',
   priority = 1000,
   config = function()
-    vim.cmd.colorscheme 'kanagawa-wave'
+    vim.o.background = 'dark' -- or "light" for light mode
+    vim.cmd.colorscheme 'gruvbox'
   end,
 }
