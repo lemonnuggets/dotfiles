ce546e14068219ca18f2ae848a1e4cb879a6ac61
lemonnuggets <63046776+lemonnuggets@users.noreply.github.com>
2025-06-15 21:38:41 +0530

    feat(nvim): show only one sign in sign column with gitsigns

--- dot_config/nvim/lua/plugins/gitsigns.lua
+++ dot_config/nvim/lua/plugins/gitsigns.lua
@@ -1,3 +1,51 @@
+function show_only_one_sign_in_sign_column()
+  ---custom namespace
+  local ns = vim.api.nvim_create_namespace 'severe-diagnostics'
+
+  ---reference to the original handler
+  local orig_signs_handler = vim.diagnostic.handlers.signs
+
+  ---Overriden diagnostics signs helper to only show the single most relevant sign
+  ---@see `:h diagnostic-handlers`
+  vim.diagnostic.handlers.signs = {
+    show = function(_, bufnr, _, opts)
+      -- get all diagnostics from the whole buffer rather
+      -- than just the diagnostics passed to the handler
+      local diagnostics = vim.diagnostic.get(bufnr)
+
+      local filtered_diagnostics = filter_diagnostics(diagnostics)
+
+      -- pass the filtered diagnostics (with the
+      -- custom namespace) to the original handler
+      orig_signs_handler.show(ns, bufnr, filtered_diagnostics, opts)
+    end,
+
+    hide = function(_, bufnr)
+      orig_signs_handler.hide(ns, bufnr)
+    end,
+  }
+
+  filter_diagnostics = function(diagnostics)
+    if not diagnostics then
+      return {}
+    end
+
+    -- find the "worst" diagnostic per line
+    local most_severe = {}
+    for _, cur in pairs(diagnostics) do
+      local max = most_severe[cur.lnum]
+
+      -- higher severity has lower value (`:h diagnostic-severity`)
+      if not max or cur.severity < max.severity then
+        most_severe[cur.lnum] = cur
+      end
+    end
+
+    -- return list of diagnostics
+    return vim.tbl_values(most_severe)
+  end
+end
+
 return {
   -- Adds git related signs to the gutter, as well as utilities for managing changes
   'lewis6991/gitsigns.nvim',
@@ -10,7 +58,9 @@ return {
       topdelete = { text = '‾' },
       changedelete = { text = '~' },
     },
+    -- sign_priority = 100,
     on_attach = function(bufnr)
+      -- show_only_one_sign_in_sign_column()
       local gs = package.loaded.gitsigns
 
       local function map(mode, l, r, opts)
